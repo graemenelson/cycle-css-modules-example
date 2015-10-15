47edef89343131284735a99370138d4e57045ccd
Graeme Nelson <graeme.nelson@gmail.com>
2015-10-14 18:17:14 -0700

    minor cleanup to have styles were being used in view and intent

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Cycle from '@cycle/core'
 import {h, makeDOMDriver} from '@cycle/dom'
 import styles from './main.css'
 
+function toDOMSelector(name) {
+  return `.${name.split(' ').join('.')}`
+}
+
 function intent(DOM) {
   return Cycle.Rx.Observable.merge(
-    DOM.select('.decrement').events('click').map(ev => -1),
-    DOM.select('.increment').events('click').map(ev => +1)
+    DOM.select(toDOMSelector(styles.decrement)).events('click').map(ev => -1),
+    DOM.select(toDOMSelector(styles.increment)).events('click').map(ev => +1)
   )
 }
 
@@ -13,23 +17,14 @@ function model(action$) {
   return action$.startWith(0).scan((acc,x) => acc + x)
 }
 
-function counterElement(count) {
-  return h(`p.${styles.counter}`, `Counter: ${count}`)
-}
-
-function buttonElement(label, classes) {
-  return h(`button.${classes.join('.')}`, label)
-}
-
 function view(state$) {
-  let buttonStyleClass = styles.button
   return {
     DOM: state$.map(count =>
         h('div', [
-          counterElement(count),
-          h(`div.${styles.actions}`, [
-            buttonElement('Decrement', ['decrement', buttonStyleClass]),
-            buttonElement('Increment', ['increment', buttonStyleClass]),
+          h('p', {className: styles.counter}, `Counter: ${count}`),
+          h('div', [
+            h('button', {className: styles.decrement}, 'Decrement'),
+            h('button', {className: styles.increment}, 'Increment')
           ])
         ])
       )
